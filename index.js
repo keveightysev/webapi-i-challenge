d4c8739ba39f1618514ae9ccd276685b41218258
Kevin Smith <keveightysev@gmail.com>
2019-03-18 14:19:46 -0500

    set up server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// implement your API here
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.listen(4000, () => {
+    console.log('\n*** Server listening on port 4000 ***')
+});
