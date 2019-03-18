e20a3298e8b8c8018f1cc57bee511f3fa57d9525
Kevin Smith <keveightysev@gmail.com>
2019-03-18 14:27:14 -0500

    GET working

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const db = require('./data/db.js');
 
 const server = express();
 
 server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({ message: err.message });
+        });
+});
 
 server.listen(4000, () => {
     console.log('\n*** Server listening on port 4000 ***')
