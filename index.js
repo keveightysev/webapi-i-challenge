98d756701d2dd01cb8a0c6237141a9ef6c1ef669
Kevin Smith <keveightysev@gmail.com>
2019-03-18 14:32:03 -0500

    GET by ID working

--- index.js
+++ index.js
@@ -11,9 +11,24 @@ server.get('/api/users', (req, res) => {
             res.status(200).json(users);
         })
         .catch(err => {
-            res.status(500).json({ message: err.message });
+            res.status(500).json({ error: "The users information could not be retrieved." });
         });
 });
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+        .then(user => {
+            if (user) {
+                res.status(200).json(user);
+            } else {
+                res.status(404).json({ message: "The user with the specified ID does not exist." });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The user information could not be retrieved." });
+        });
+})
 
 server.listen(4000, () => {
     console.log('\n*** Server listening on port 4000 ***')
